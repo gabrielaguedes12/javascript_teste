d08394871481ed258bd08699b9dba0096a1f2b24
gabrielaguedes12 <gabr1.profissional@gmail.com>
2025-10-20 19:19:36 -0300

    teste

---
+++ script.js
@@ -0,0 +1,25 @@
+console.log("Olá, mundo!");
+
+//variavel: são usadas para armazenar os dados dos usuarios
+//estrutura
+//tipo da variavel + nome da variavel + valor;
+
+//tipos de variaveis
+// var, let e const
+
+let idade = 18;
+console.log(idade);
+
+idade = 19;
+console.log(idade);
+
+// let é uma variavel que pode ser reatribuida
+let nome ="Marina";
+console.log(nome);
+
+//const
+const cpf = 123455678911;
+console.log(cpf);
+
+const sobrenome = "Pereira";
+console.log(sobrenome);
