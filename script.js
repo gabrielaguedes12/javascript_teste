3629d572f08bb3cd06807fdd622bda6bf53870b0
gabrielaguedes12 <gabr1.profissional@gmail.com>
2025-10-29 18:53:28 -0300

    teste

--- script.js
+++ script.js
@@ -32,3 +32,50 @@ console.log(n1 + n2);
 
 n2 = "3";
 console.log(n1 + n2);
+
+
+// variaveis são espaços na memória onde podemos armazenar dados
+// regras para criar nomes de variáveis:
+// 1. não pode começar com número
+// 2. não pode ter espaços
+// 3. não pode usar caracteres especiais (ex: @, #, $, %, &, *)
+// 4. não pode usar palavras reservadas (ex: let, const, var, if, else, function, return, etc)
+// 5. usar camelCase para nomes compostos (ex: nomeCompleto, idadeUsuario, etc)
+// 6. Javascript é case sensitive (maiusculas e minusculas fazem diferença)
+
+// 3 tipos de variaveis 
+// var (não usar mais)
+// let (usar quando o valor da variável pode mudar)
+// const (usar quando o valor da variável não pode mudar)
+
+let numeroCelular = "(11) 99999-9999";
+console.log(numeroCelular);
+
+numeroCelular = "(11) 88888-8888";
+console.log(numeroCelular);
+
+const nacionalidade = "brasileira";
+console.log(nacionalidade);
+
+// nacionalidade = "argentina"; // isso vai dar erro, pois const não pode ser reatribuída
+
+// tipos de dados
+// string "" ou '' que são textos, caracteres, palavras
+let nomeDoVisitante = "Surpresa!";
+console.log(nomeDoVisitante);
+
+// number -> dado numerico usado para armazenar numero = 123, 12.5, -10 que são números
+let quantidadeDeLivros = 10;
+console.log(quantidadeDeLivros);
+
+// boolean -> é um tipo de dado que usamos para true ou false (verdadeiro ou falso)
+let visitanteAtivo = true;
+console.log(visitanteAtivo);
+
+// null -> é um valor nulo, ou seja, a variável não possui valor nenhum
+let saldoBancario = null;
+console.log(saldoBancario);
+
+// undefined -> é quando criamos uma variável mas não atribuímos nenhum valor a ela
+let presencaDoVisitante;
+console.log(presencaDoVisitante);
